5f4eaa5c0d51c96e7da150d70088c0d50fe27a5d
Miguel <miguel.gebhardt@bechtle.com>
2024-05-08 17:11:28 +0200

    feat: :sparkles: added landinghero to startpage

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    <p>Hello Nuxt</p>
+    <ULandingHero
+    title="Handball Berichte"
+    description="Diese Seite zeigt verschiedene Handballberichte von verschiedenen Vereinen an. Die Berichte werden dabei von den Vereinshompages gelesen."
+    orientation="horizontal"
+  >
+    <template #default>
+      <LazyNuxtImg
+        src="/logo.png"
+        width="auto"
+        height="25%"
+      />
+    </template>
+  </ULandingHero>
   </div>
 </template>
 
